04dec222d77833dda659129f594054f5a289dd3a
Kreyno93 <michael.wagener@neuefische.de>
2026-05-28 11:51:20 +0200

    [ADD] adding multiplication

--- calculator.py
+++ calculator.py
@@ -1,6 +1,11 @@
 def addition(a, b):
     return a + b
 
+
+def multiplication(a, b):
+    return a * b
+
+
 def main():
     # Ask the user for two numbers
     num1 = float(input("Enter the first number: "))
@@ -8,9 +13,12 @@ def main():
 
     # Perform addition
     result = addition(num1, num2)
-    
+    result_multiplication = multiplication(num1, num2)
+
     # Display the result
     print(f"The sum of {num1} and {num2} is: {result}")
+    print(f"The product of {num1} and {num2} is: {result_multiplication}")
+
 
 if __name__ == "__main__":
     main()
